828661881d3ed0be42ee908b647f874d5a4c1ca6
Yurii Volkov <sir.volkov@outlook.com>
2026-03-29 18:14:57 +0300

    task-09 complete

--- js/task-09.js
+++ js/task-09.js
@@ -10,8 +10,19 @@
  * Use the getRandomHexColor function to generate a random color.
  */
 
+const spanEl = document.querySelector(".color");
+const btnEl = document.querySelector(".change-color");
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+}
+
+btnEl.addEventListener("click", onBtnClickChangeColor);
+
+function onBtnClickChangeColor () {
+  const color = getRandomHexColor();
+  document.body.style.backgroundColor = color;
+  spanEl.textContent = color;
 }
